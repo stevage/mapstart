715ca6a06095709e84906cd5ccf2e67be708bfa0
Steve Bennett <me@stevebennett.me>
2022-10-31 21:49:04 +1100

    Init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+.about
+  h1 HEllo
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
